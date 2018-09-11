aceba660313415e7ba0dbd793f3edf6dc7b75028
Anyon <zoujingli@qq.com>
2018-09-11 15:44:26 +0800

    [更新]修改会话配置

--- config/session.php
+++ config/session.php
@@ -13,17 +13,15 @@
 // +----------------------------------------------------------------------
 
 /* 定义Session会话字段名 */
-$session_name = 's' . substr(md5(__DIR__), -8);
-$session_path = env('runtime_path') . 'sess' . DIRECTORY_SEPARATOR;
-file_exists($session_path) || mkdir($session_path, 0755, true);
+$path = env('runtime_path') . 'sess' . DIRECTORY_SEPARATOR;
+file_exists($path) || mkdir($path, 0755, true);
+$name = 's' . substr(md5(__DIR__), -8);
 
 /* 定义Session会话参数 */
 return [
-    'id'             => '',
-    'type'           => '',
     'prefix'         => 'ta',
     'auto_start'     => true,
-    'path'           => $session_path,
-    'name'           => $session_name,
-    'var_session_id' => $session_name,
+    'path'           => $path,
+    'name'           => $name,
+    'var_session_id' => $name,
 ];
