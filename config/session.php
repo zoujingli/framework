cfebb28d5f683eaa69fd06c97bdcecaa6a671b6f
Anyon <zoujingli@qq.com>
2018-09-30 11:13:59 +0800

    [更新]修改session配置

--- config/session.php
+++ config/session.php
@@ -12,16 +12,17 @@
 // | github开源项目：https://github.com/zoujingli/framework
 // +----------------------------------------------------------------------
 
+
 /* 定义会话路径 */
-$path = env('runtime_path') . 'sess' . DIRECTORY_SEPARATOR;
-file_exists($path) || mkdir($path, 0755, true);
-$name = 's' . substr(md5(__DIR__), -8);
+$_path_ = env('runtime_path') . 'sess' . DIRECTORY_SEPARATOR;
+file_exists($_path_) || mkdir($_path_, 0755, true);
+$_name_ = 's' . substr(md5(__DIR__), -8);
 
 /* 配置会话参数 */
 return [
-    'prefix'         => 'ta',
+    'prefix'         => 'fw',
     'auto_start'     => true,
-    'path'           => $path,
-    'name'           => $name,
-    'var_session_id' => $name,
+    'path'           => $_path_,
+    'name'           => $_name_,
+    'var_session_id' => $_name_,
 ];
