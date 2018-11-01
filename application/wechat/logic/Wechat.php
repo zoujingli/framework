717453a14d619fdd22b7c87788fe72a1fdeb6725
Anyon <zoujingli@qq.com>
2018-11-01 11:42:07 +0800

    [更新]修改微信支付配置

--- application/wechat/logic/Wechat.php
+++ application/wechat/logic/Wechat.php
@@ -30,17 +30,22 @@ class Wechat
      */
     public static function config()
     {
-        return [
+        $options = [
             'token'          => sysconf('wechat_token'),
             'appid'          => sysconf('wechat_appid'),
             'appsecret'      => sysconf('wechat_appsecret'),
             'encodingaeskey' => sysconf('wechat_encodingaeskey'),
             'mch_id'         => sysconf('wechat_mch_id'),
-            'mch_key'        => sysconf('wechat_partnerkey'),
-            'ssl_cer'        => sysconf('wechat_cert_cert'),
-            'ssl_key'        => sysconf('wechat_cert_key'),
+            'mch_key'        => sysconf('wechat_mch_key'),
             'cachepath'      => env('cache_path') . 'wechat' . DIRECTORY_SEPARATOR,
         ];
+        if (sysconf('wechat_mch_ssl_type') === 'p12') {
+            $options['ssl_p12'] = sysconf('wechat_mch_ssl_p12');
+        } else {
+            $options['ssl_key'] = sysconf('wechat_mch_ssl_key');
+            $options['ssl_cer'] = sysconf('wechat_mch_ssl_cer');
+        }
+        return $options;
     }
 
     /**
