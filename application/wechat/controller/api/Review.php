fbcddc6f1aa57d2291971e2f6cacff7d21929ac1
邹景立 <zoujingli@qq.com>
2018-11-13 23:30:49 +0800

    [更新]增加图文预览

--- application/wechat/controller/api/Review.php
+++ application/wechat/controller/api/Review.php
@@ -16,13 +16,36 @@ namespace app\wechat\controller\api;
 
 use app\wechat\logic\Media;
 use library\Controller;
+use think\Db;
 
 class Review extends Controller
 {
 
+    /**
+     * 显示图文列表
+     * @param integer $id
+     * @return mixed
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
     public function news($id)
     {
         $this->news = Media::news($id);
+        return $this->fetch();
+    }
+
+    /**
+     * 显示图文内容
+     * @param integer $id
+     * @return mixed
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function view($id)
+    {
+        $this->info = Db::name('WechatNewsArticle')->where(['id' => $id])->find();
         return $this->fetch();
     }
 
