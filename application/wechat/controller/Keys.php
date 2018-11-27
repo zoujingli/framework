f14baeddc59034201152c9fa3037c217562e8c14
Anyon <zoujingli@qq.com>
2018-11-27 11:44:16 +0800

    [更新]修改回复规则

--- application/wechat/controller/Keys.php
+++ application/wechat/controller/Keys.php
@@ -33,6 +33,15 @@ class Keys extends Controller
     protected $table = 'WechatKeys';
 
     /**
+     * 消息类型
+     * @var array
+     */
+    protected $types = [
+        'keys'  => '关键字', 'image' => '图片', 'news' => '图文',
+        'music' => '音乐', 'text' => '文字', 'video' => '视频', 'voice' => '语音',
+    ];
+
+    /**
      * 显示关键字列表
      * @return array|string
      */
@@ -63,13 +72,9 @@ class Keys extends Controller
     protected function _index_page_filter(&$data)
     {
         try {
-            $types = [
-                'keys' => '关键字', 'image' => '图片', 'news' => '图文', 'music' => '音乐',
-                'text' => '文字', 'video' => '视频', 'voice' => '语音',
-            ];
             foreach ($data as &$vo) {
                 $vo['qrc'] = url('@wechat/keys/index') . "?action=qrc&keys={$vo['keys']}";
-                $vo['type'] = isset($types[$vo['type']]) ? $types[$vo['type']] : $vo['type'];
+                $vo['type'] = isset($this->types[$vo['type']]) ? $this->types[$vo['type']] : $vo['type'];
             }
         } catch (\Exception $e) {
             $this->error($e->getMessage());
@@ -118,6 +123,56 @@ class Keys extends Controller
     public function resume()
     {
         $this->_save($this->table, ['status' => '1']);
+    }
+
+    /**
+     * 关注默认回复
+     * @return array|string
+     */
+    public function subscribe()
+    {
+        $this->title = '编辑关注回复规则';
+        return $this->_form($this->table, 'form', 'keys', [], ['keys' => 'subscribe']);
+    }
+
+
+    /**
+     * 无配置默认回复
+     * @return array|string
+     */
+    public function defaults()
+    {
+        $this->title = '编辑默认回复规则';
+        return $this->_form($this->table, 'form', 'keys', [], ['keys' => 'default']);
+    }
+
+    /**
+     * 添加数据处理
+     * @param array $data
+     */
+    protected function _form_filter(array &$data)
+    {
+        if ($this->request->isPost() && isset($data['keys'])) {
+            $db = Db::name($this->table)->where('keys', $data['keys']);
+            empty($data['id']) || $db->where('id', 'neq', $data['id']);
+            if ($db->count() > 0) $this->error('关键字已经存在，请使用其它关键字！');
+        }
+    }
+
+    /**
+     * 编辑结果处理
+     * @param $result
+     */
+    protected function _form_result($result)
+    {
+        if ($result !== false) {
+            list($url, $keys) = ['', $this->request->post('keys')];
+            if (!in_array($keys, ['subscribe', 'default'])) {
+                $url = url('@admin') . '#' . url('wechat/keys/index') . '?spm=' . $this->request->get('spm');
+            }
+            $this->success('恭喜, 关键字保存成功!', $url);
+        }
+        $this->error('关键字保存失败, 请稍候再试!');
     }
 
 }
