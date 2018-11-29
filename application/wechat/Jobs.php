0ce560f73ec5bc0bdf766eedf81e822d15e0bb5c
Anyon <zoujingli@qq.com>
2018-11-29 13:50:33 +0800

    Update Jobs.php

--- application/wechat/Jobs.php
+++ application/wechat/Jobs.php
@@ -42,29 +42,26 @@ class Jobs extends Queue
             $wechat = \We::WeChatUser(Wechat::config());
             $next = ''; // 获取远程粉丝
             $this->writeln('准备同步粉丝列表...');
-            while (true) if (is_array($result = $wechat->getUserList($next)) && !empty($result['data']['openid'])) {
-                foreach (array_chunk($result['data']['openid'], 100) as $chunk) {
-                    if (is_array($list = $wechat->getBatchUserInfo($chunk)) && !empty($list['user_info_list'])) {
+            while (is_array($result = $wechat->getUserList($next)) && !empty($result['data']['openid'])) {
+                foreach (array_chunk($result['data']['openid'], 100) as $chunk)
+                    if (is_array($list = $wechat->getBatchUserInfo($chunk)) && !empty($list['user_info_list']))
                         foreach ($list['user_info_list'] as $user) Fans::set($user, $appid);
-                    }
-                }
+                $next = $result['next_openid'];
                 if (in_array($result['next_openid'], $result['data']['openid'])) break;
-                else $next = $result['next_openid'];
             }
             $next = ''; // 同步粉丝黑名单
             $this->writeln('准备同步粉丝黑名单列表...');
-            while (true) if (is_array($result = $wechat->getBlackList($next)) && !empty($result['data']['openid'])) {
+            while (is_array($result = $wechat->getBlackList($next)) && !empty($result['data']['openid'])) {
                 foreach (array_chunk($result['data']['openid'], 100) as $chunk) {
                     $where = [['is_black', 'eq', '0'], ['openid', 'in', $chunk]];
-                    Db::name('WechatFans')->failException(true)->where($where)->update(['is_black' => '1']);
+                    Db::name('WechatFans')->where($where)->update(['is_black' => '1']);
                 }
+                $next = $result['next_openid'];
                 if (in_array($result['next_openid'], $result['data']['openid'])) break;
-                else $next = $result['next_openid'];
             }
             // 同步粉丝标签列表
             $this->writeln('准备同步粉丝标签列表...');
-            $wechat = \We::WeChatTags(Wechat::config());
-            if (is_array($list = $wechat->getTags()) && !empty($list['tags'])) {
+            if (is_array($list = \We::WeChatTags(Wechat::config())->getTags()) && !empty($list['tags'])) {
                 foreach ($list['tags'] as &$tag) $tag['appid'] = $appid;
                 Db::name('WechatFansTags')->where('1=1')->delete();
                 Db::name('WechatFansTags')->insertAll($list['tags']);
