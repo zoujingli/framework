475f149e7717ab7544c8cd9c13bf7e03647a7954
Anyon <zoujingli@qq.com>
2018-11-29 10:19:37 +0800

    Update middleware.php

--- application/middleware.php
+++ application/middleware.php
@@ -17,14 +17,13 @@ use library\tools\Node;
 use think\Db;
 
 return [function (\think\Request $request, \Closure $next) {
-    list($module, $controller, $action) = [$request->module(), $request->controller(), $request->action()];
-    $node = Node::parseString("{$module}/{$controller}/{$action}");
-    $info = Db::name('SystemNode')->cache(true, 30)->where(['node' => $node])->find();
-    $access = ['is_auth' => $info['is_auth'], 'is_login' => $info['is_auth'] ? 1 : $info['is_login']];
+    $map = ['node' => ($node = Node::current())];
+    $auth = Db::name('SystemNode')->cache(true, 30)->field('is_auth,is_login')->where($map)->find();
+    $access = ['is_auth' => $auth['is_auth'], 'is_login' => $auth['is_auth'] ? 1 : $auth['is_login']];
     // 登录状态检查
-    if (!empty($access['is_login']) && !session('user')) {
-        $msg = ['code' => 0, 'msg' => '抱歉，您还没有登录获取访问权限！', 'url' => url('@admin/login')];
-        return $request->isAjax() ? json($msg) : redirect($msg['url']);
+    if (!empty($access['is_login']) && !Auth::isLogin()) {
+        $message = ['code' => 0, 'msg' => '抱歉，您还没有登录获取访问权限！', 'url' => url('@admin/login')];
+        return $request->isAjax() ? json($message) : redirect($message['url']);
     }
     // 访问权限检查
     if (!empty($access['is_auth']) && !Auth::checkAuthNode($node)) {
