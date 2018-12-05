10964679a885aa0c1d49cb0cc2b28b9df1fe4df8
邹景立 <zoujingli@qq.com>
2018-12-05 23:05:11 +0800

    Update Update.php

--- application/admin/logic/Update.php
+++ application/admin/logic/Update.php
@@ -61,11 +61,11 @@ class Update
         foreach ($local as $t) $_local[$t['name']] = $t;
         unset($serve, $local);
         // 数据差异计算
-        foreach ($_serve as $s) if (isset($_local[$s['name']])) array_push($_new, [
-            'type' => $s['hash'] === $_local[$s['name']]['hash'] ? null : 'mod',
-            'name' => $s['name'], 'serve_hash' => $s['hash'], 'local_hash' => $_local[$s['name']]['hash'],
+        foreach ($_serve as $t) if (isset($_local[$t['name']])) array_push($_new, [
+            'type' => $t['hash'] === $_local[$t['name']]['hash'] ? null : 'mod',
+            'name' => $t['name'], 'serve_hash' => $t['hash'], 'local_hash' => $_local[$t['name']]['hash'],
         ]);
-        else array_push($_new, ['type' => 'add', 'name' => $s['name'], 'serve_hash' => $s['hash']]);
+        else array_push($_new, ['type' => 'add', 'name' => $t['name'], 'serve_hash' => $t['hash']]);
         // 数据增量计算
         foreach ($_local as $t) if (!isset($_serve[$t['name']])) array_push($_new, [
             'type' => 'del', 'name' => $t['name'], 'local_hash' => $t['hash']
