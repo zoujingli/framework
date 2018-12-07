ba65be26478c736299d82a4be9a5bc25834c9892
Anyon <zoujingli@qq.com>
2018-12-07 11:52:02 +0800

    Update Update.php

--- application/admin/controller/api/Update.php
+++ application/admin/controller/api/Update.php
@@ -53,8 +53,7 @@ class Update extends Controller
     public function read($encode)
     {
         $file = env('root_path') . decode($encode);
-        dump($file);
-        if (file_exists($file)) $this->error('获取文件内容失败！');
+        if (!file_exists($file)) $this->error('获取文件内容失败！');
         $content = base64_encode(file_get_contents($file));
         $this->success('获取文件内容成功！', ['format' => 'base64', 'content' => $content]);
     }
