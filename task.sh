99092ba6c18dcd6a7048adc521988d93ec50049f
Anyon <zoujingli@qq.com>
2018-12-15 15:56:36 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 command="php $(cd `dirname $0`;pwd)/think queue:listen"
 ps aux|grep -v grep|grep "$command" > /dev/null
-if [ $? != 0 ]; then echo "\n没有发现进程，正在启动进程...\n"
+if [ $? != 0 ]; then echo -e "\n没有发现进程，正在启动进程...\n"
 $command > /dev/null 2>&1 &
 ps aux|grep -v grep|grep "$command" > /dev/null
-if [ $? = 0 ]; then echo "### 进程启动成功 ### \n";else echo "### 进程启动失败 ###\n";fi
-else echo "\n进程已经存在，不需要重新启动进程\n";fi
+if [ $? = 0 ]; then echo -e "### 进程启动成功 ### \n";else echo -e "### 进程启动失败 ###\n";fi
+else echo -e "\n进程已经存在，不需要重新启动进程\n";fi
