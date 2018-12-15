27756995d1004dfd2273b67e87816c5acea9223a
Anyon <zoujingli@qq.com>
2018-12-15 15:51:41 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -4,5 +4,5 @@ ps aux|grep -v grep|grep "$command" > /dev/null
 if [ $? != 0 ]; then echo "没有发现进程，正在启动进程..."
 $command > /dev/null 2>&1 &
 ps aux|grep -v grep|grep "$command" > /dev/null
-if [ $? = 0 ]; then echo "进程启动成功！";else echo "进程启动失败！";fi
+if [ $? = 0 ]; then echo -e "\032进程启动成功！\032[0m";else echo -e "\031进程启动失败！\031[0m";fi
 else echo "进程已经存在，不需要重新启动进程！";fi
