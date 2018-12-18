3adea57bbde839f123d5aa168d8898f9a5010f92
Anyon <zoujingli@qq.com>
2018-12-18 19:24:45 +0800

    Create DoorUser.php

---
+++ application/store/controller/api/DoorUser.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\store\controller\api;
+
+use library\Controller;
+
+/**
+ * Class DoorUser
+ * @package app\store\controller\api
+ */
+class DoorUser extends Controller
+{
+    public function add()
+    {
+
+    }
+}
