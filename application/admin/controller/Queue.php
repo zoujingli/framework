29454e6217e768ea608bf21a7679f2e98b6d657a
Anyon <zoujingli@qq.com>
2019-01-05 13:35:30 +0800

    [更新]修改任务消息

--- application/admin/controller/Queue.php
+++ application/admin/controller/Queue.php
@@ -43,7 +43,7 @@ class Queue extends Controller
         $this->title = '系统任务管理';
         $this->uris = Db::name($this->table)->distinct(true)->column('uri');
         $this->titles = Db::name($this->table)->distinct(true)->column('title');
-        return $this->_query($this->table)->equal('status,title,uri')->dateBetween('create_at')->order('id desc')->page();
+        return $this->_query($this->table)->equal('status,title,uri')->dateBetween('create_at,status_at')->order('id desc')->page();
     }
 
     /**
