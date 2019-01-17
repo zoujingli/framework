59b4ee49312d4058d567ddc6de0ace5d4204eedd
Anyon <zoujingli@qq.com>
2019-01-17 16:32:15 +0800

    Create Order.php

---
+++ application/store/controller/api/member/Order.php
@@ -0,0 +1,51 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | framework
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2018 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
+// +----------------------------------------------------------------------
+// | 官方网站: http://framework.thinkadmin.top
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | github开源项目：https://github.com/zoujingli/framework
+// +----------------------------------------------------------------------
+
+namespace app\store\controller\api\member;
+
+use app\store\controller\api\Member;
+
+/**
+ * 订单接口管理
+ * Class Order
+ * @package app\store\controller\api\member
+ */
+class Order extends Member
+{
+
+    public function set()
+    {
+
+    }
+
+    public function gets()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+
+    public function cancel()
+    {
+
+    }
+
+    public function confirm()
+    {
+
+    }
+}
