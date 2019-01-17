9faa5f75667951241b7fcfa5201074ecd73667b9
Anyon <zoujingli@qq.com>
2019-01-17 18:37:29 +0800

    Update Order.php

--- application/store/controller/api/member/Order.php
+++ application/store/controller/api/member/Order.php
@@ -15,6 +15,7 @@
 namespace app\store\controller\api\member;
 
 use app\store\controller\api\Member;
+use think\Db;
 
 /**
  * 订单接口管理
@@ -49,18 +50,55 @@ class Order extends Member
     }
 
     /**
-     * 取消订单
+     * 订单取消
+     * @throws \think\Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * @throws \think\exception\PDOException
      */
     public function cancel()
     {
-
+        $where = [
+            'mid'      => $this->member['id'],
+            'order_no' => $this->request->post('order_no'),
+        ];
+        $order = Db::name('StoreOrder')->where($where)->find();
+        if (empty($order)) $this->error('待取消的订单不存在，请稍候再试！');
+        if (in_array($order['status'], ['1', '2'])) {
+            $result = Db::name('StoreOrder')->where($where)->update([
+                'status'       => '0',
+                'cancel_state' => '1',
+                'cancel_at'    => date('Y-m-d H:i:s'),
+                'cancel_desc'  => '用户主动取消订单！',
+            ]);
+            if ($result !== false) $this->success('订单取消成功！');
+            $this->error('订单取消失败，请稍候再试！');
+        }
+        $this->error('该订单状态不允许取消哦~');
     }
 
     /**
      * 订单确认收货
+     * @throws \think\Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * @throws \think\exception\PDOException
      */
     public function confirm()
     {
-
+        $where = [
+            'mid'      => $this->member['id'],
+            'order_no' => $this->request->post('order_no'),
+        ];
+        $order = Db::name('StoreOrder')->where($where)->find();
+        if (empty($order)) $this->error('待确认的订单不存在，请稍候再试！');
+        if (in_array($order['status'], ['4'])) {
+            $result = Db::name('StoreOrder')->where($where)->update(['status' => '5']);
+            if ($result !== false) $this->success('订单确认成功！');
+            $this->error('订单取确认失败，请稍候再试！');
+        }
+        $this->error('该订单状态不允许确认哦~');
     }
 }
