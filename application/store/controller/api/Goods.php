9aaa216bb71384bf13a1cc88de3696fd55d4c74a
Anyon <zoujingli@qq.com>
2019-01-23 11:05:25 +0800

    Update Goods.php

--- application/store/controller/api/Goods.php
+++ application/store/controller/api/Goods.php
@@ -40,7 +40,7 @@ class Goods extends Controller
         if ($this->request->has('cate_id', 'post', true)) {
             $where[] = ['cate_id', 'eq', $this->request->post('cate_id')];
         }
-        $field = 'id,title,logo,specs,lists,image,content,number_sales,number_stock';
+        $field = 'id,title,logo,specs,lists,cate_id,image,content,number_sales,number_stock';
         $list = Db::name('StoreGoods')->field($field)->where($where)->order('sort asc,id desc')->select();
         $goodsList = Db::name('StoreGoodsList')->whereIn('goods_id', array_unique(array_column($list, 'id')))->select();
         foreach ($list as &$vo) {
@@ -65,7 +65,7 @@ class Goods extends Controller
     {
         $goods_id = input('goods_id');
         $where = ['is_deleted' => '0', 'status' => '1', 'id' => $goods_id];
-        $field = 'id,title,logo,specs,lists,image,content,number_sales,number_stock';
+        $field = 'id,title,logo,specs,lists,cate_id,image,content,number_sales,number_stock';
         $goods = Db::name('StoreGoods')->field($field)->where($where)->find();
         if (empty($goods)) $this->error('指定商品不存在，请更换商品ID重试！');
         $goods['image'] = explode('|', $goods['image']);
@@ -75,5 +75,19 @@ class Goods extends Controller
         if (empty($goods['list'])) $this->error('指定商品规格不存在，请更换商品ID重试！');
         $this->success('获取商品信息成功！', $goods);
     }
+
+    /**
+     * 获取商品分类信息
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function cate()
+    {
+        $where = ['is_deleted' => '0', 'status' => '1'];
+        $list = Db::name('StoreGoodsCate')->where($where)->order('sort asc,id desc')->select();
+        $this->success('获取商品分类成功！', ['list' => $list]);
+    }
+
 
 }
