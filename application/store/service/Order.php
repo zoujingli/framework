c667814f4174c4baf19575d29fa8e7a804456729
Anyon <zoujingli@qq.com>
2019-01-24 16:29:47 +0800

    [更新]修改页面

--- application/store/service/Order.php
+++ application/store/service/Order.php
@@ -14,8 +14,6 @@
 
 namespace app\store\service;
 
-use think\Db;
-
 /**
  * 订单服务管理器
  * Class Order
@@ -24,71 +22,4 @@ use think\Db;
 class Order
 {
 
-    /**
-     * 获取订单实际支付金额
-     * @param array $member
-     * @param array $orderSpec
-     * @param integer $number
-     * @return array|mixed
-     */
-    public static function buildOrderPrice($type = 0, $member = [], $orderSpec = [], $number = 1)
-    {
-        switch (intval($member['vip_level'])) {
-            case 0: # 游客
-                $orderSpec['price_real'] = $orderSpec['price_selling'] * $number;
-                return $orderSpec;
-            case 1: # 临时会员
-                switch (intval($orderSpec['vip_mod'])) {
-                    case 0: # 没有权限
-                    case 1: # 临时会员，都按会员价格计算
-                        $orderSpec['price_real'] = $orderSpec['price_selling'] * $number;
-                        return $orderSpec;
-                    case 2: # 临时会员升VIP1，按销售价格计算
-                        $orderSpec['price_real'] = $orderSpec['price_selling'] * $number;
-                        return $orderSpec;
-                }
-                return $orderSpec['price_member'];
-            case 2: # VIP1会员
-                switch (intval($orderSpec['vip_mod'])) {
-                    case 0: # 没有权限
-                    case 1: # 临时会员
-                        $orderSpec['price_real'] = $orderSpec['price_member'] * $number;
-                        return $orderSpec;
-                    case 2: # 普通会员
-                        $diffPrice = $orderSpec['price_selling'] - $orderSpec['vip_discount'];
-                        $orderSpec['price_real'] = ($diffPrice < 0 ? 0 : $diffPrice) * $number;
-                        $orderSpec['discount_price'] = $orderSpec['vip_discount'];
-                        $orderSpec['discount_desc'] = 'VIP1升VIP2优惠金额';
-                        return $orderSpec;
-                }
-                return $orderSpec;
-            case 3: # VIP2会员
-                switch (intval($orderSpec['vip_mod'])) {
-                    case 0: # 没有权限
-                    case 1: # 临时会员
-                        $orderSpec['price_real'] = $orderSpec['price_member'] * $number;
-                        return $orderSpec;
-                    case 2: # 普通会员
-                        $orderSpec['price_real'] = $orderSpec['price_selling'];
-                        $orderSpec['discount_price'] = '0.00';
-                        $orderSpec['discount_desc'] = 'VIP2续期不享受优惠';
-                        return $orderSpec;
-                }
-        }
-
-    }
-
-    /**
-     * 更新会员级别信息
-     * @param string $order_no
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\ModelNotFoundException
-     * @throws \think\exception\DbException
-     */
-    public static function updateMember($order_no)
-    {
-        $where = ['order_no' => $order_no, 'pay_state' => '1'];
-        $order = Db::name('StoreOrder')->where($where)->find();
-
-    }
 }
