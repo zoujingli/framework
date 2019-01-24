981d2f6f617b92a3f8b7e5a7cf467ecd55e35a8a
Anyon <zoujingli@qq.com>
2019-01-24 14:29:43 +0800

    Update Order.php

--- application/store/controller/api/member/Order.php
+++ application/store/controller/api/member/Order.php
@@ -43,8 +43,8 @@ class Order extends Member
         if (empty($rule)) $this->error('下单商品规则不能为空！');
         // 订单处理
         list($order, $orderList) = [[], []];
-        $order['type'] = $type;
         $order['mid'] = $this->mid;
+        $order['type'] = $type;
         $order['order_no'] = Data::uniqidNumberCode(12);
         foreach (explode('||', $rule) as $item) {
             list($goods_id, $goods_spec, $number) = explode('@', $item);
