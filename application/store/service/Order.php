fb45e372c3c12dc5209e4f3d113909c021ce5a2b
Anyon <zoujingli@qq.com>
2019-01-24 18:52:38 +0800

    Update Order.php

--- application/store/service/Order.php
+++ application/store/service/Order.php
@@ -14,6 +14,8 @@
 
 namespace app\store\service;
 
+use think\Db;
+
 /**
  * 订单服务管理器
  * Class Order
@@ -21,5 +23,25 @@ namespace app\store\service;
  */
 class Order
 {
+    /**
+     * 根据订单号升级会员等级
+     * @param integer $mid 会员ID
+     * @param string $order_no 订单单号
+     * @return boolean
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public static function update($mid, $order_no)
+    {
+        $member = Db::name('StoreMember')->where(['id' => $mid])->find();
+        if (empty($member)) return false;
+        $order = Db::name('StoreOrder')->where(['order_no' => $order_no, 'pay_state' => '1'])->find();
+        if (empty($order)) return false;
+        $orderList = Db::name('StoreOrderList')->where(['order_no' => $order_no])->whereIn('vip_mod', ['1', '2'])->column('vip_mod');
+        if (empty($orderList)) return false;
+        $isUsedOrder = Db::name('StoreMemberHistory')->where(['order_no' => $order_no])->count() > 0;
+        if ($isUsedOrder) return false;
 
+    }
 }
