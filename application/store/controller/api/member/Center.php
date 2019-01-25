f6e9ab314b6df22b8a03a4d6efb9d6d154917901
Anyon <zoujingli@qq.com>
2019-01-25 17:16:25 +0800

    Update Center.php

--- application/store/controller/api/member/Center.php
+++ application/store/controller/api/member/Center.php
@@ -43,8 +43,7 @@ class Center extends Member
             $data['username'] = emoji_encode($this->request->post('username'));
         }
         if (empty($data)) $this->error('没有需要修改的数据哦！');
-        $data['id'] = $this->member['id'];
-        if (data_save('StoreMember', $data, 'id')) {
+        if (data_save('StoreMember', array_merge($data, ['id' => $this->mid]), 'id')) {
             $this->success('会员资料更新成功！', $this->getMember());
         }
         $this->error('会员资料更新失败，请稍候再试！');
