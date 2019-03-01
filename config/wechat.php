7839ef762505550bf1632c9417e26234cf2ba95c
Anyon <zoujingli@qq.com>
2019-03-01 10:05:25 +0800

    Update wechat.php

--- config/wechat.php
+++ config/wechat.php
@@ -17,10 +17,11 @@ return [
     'service_url' => 'https://framework.thinkadmin.top',
     // 小程序支付参数
     'miniapp'     => [
-        'appid'     => 'wx6bb7b70258da09c6',
-        'appsecret' => 'd47a7394a7b69c7180bd11ec8d55d234',
-        'mch_id'    => '1332187001',
-        'mch_key'   => 'A82DC5BD1F3359081049C568D8502BC5',
-        'ssl_p12'   => __DIR__ . '/cert/1332187001_20181030_cert.p12',
+        'appid'      => 'wx6bb7b70258da09c6',
+        'appsecret'  => 'd47a7394a7b69c7180bd11ec8d55d234',
+        'mch_id'     => '1332187001',
+        'mch_key'    => 'A82DC5BD1F3359081049C568D8502BC5',
+        'ssl_p12'    => __DIR__ . '/cert/1332187001_20181030_cert.p12',
+        'cache_path' => env('runtime_path') . 'wechat' . DIRECTORY_SEPARATOR,
     ],
 ];
