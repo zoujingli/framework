0c825ca00f5fe059504a241204351293b643ce11
Anyon <zoujingli@qq.com>
2019-03-13 15:37:49 +0800

    [更新]修改任务指令显示

--- application/admin/controller/Queue.php
+++ application/admin/controller/Queue.php
@@ -42,6 +42,7 @@ class Queue extends Controller
     {
         $this->title = '系统任务管理';
         $this->uris = Db::name($this->table)->distinct(true)->column('uri');
+        $this->cmd = dirname(PHP_BINARY) . DIRECTORY_SEPARATOR . 'php ' . env('root_path') . 'think xrun:task';
         $this->_query($this->table)->equal('status,title,uri')->dateBetween('create_at,status_at')->order('id desc')->page();
     }
 
