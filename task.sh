4a3fada6db6486f52128e71050b5ec3be17ff194
Anyon <zoujingli@qq.com>
2019-03-14 10:32:53 +0800

    Delete task.sh

--- task.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd `dirname $0`
-command="php $(cd `dirname $0`;pwd)/think queue:listen"
-ps aux|grep -v grep|grep "$command" > /dev/null
-if [ $? != 0 ]; then echo -e "\n没有发现进程，正在启动进程...\n"
-$command > /dev/null 2>&1 &
-ps aux|grep -v grep|grep "$command" > /dev/null
-if [ $? = 0 ]; then echo -e "   ### 进程启动成功 ###\n";else echo -e "   ### 进程启动失败 ###\n";fi
-else echo -e "\n进程已经存在，不需要重新启动进程\n";fi
