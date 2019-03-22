2fcb338141c46b1120421dfe4aeccb45e555fa2c
Anyon <zoujingli@qq.com>
2019-03-22 17:10:36 +0800

    Update AutoRun.php

--- application/store/command/AutoRun.php
+++ application/store/command/AutoRun.php
@@ -15,8 +15,6 @@
 namespace app\store\command;
 
 use think\console\Command;
-use think\console\Input;
-use think\console\Output;
 use think\Db;
 
 /**
@@ -26,52 +24,36 @@ use think\Db;
  */
 class AutoRun extends Command
 {
-    /**
-     * 配置入口
-     */
+
     protected function configure()
     {
         $this->setName('xclean:store')->setDescription('clean up invalid store records');
     }
 
-    /**
-     * 执行指令
-     * @param Input $input
-     * @param Output $output
-     * @throws \think\Exception
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\ModelNotFoundException
-     * @throws \think\exception\DbException
-     * @throws \think\exception\PDOException
-     */
-    protected function execute(Input $input, Output $output)
+    protected function execute(\think\console\Input $input, \think\console\Output $output)
     {
         # 自动取消30分钟未支付的订单
-        $count = Db::name('StoreOrder')->where([
-            ['pay_state', 'eq', '0'], ['status', 'in', ['1', '2']],
-            ['create_at', '<', date('Y-m-d H:i:s', strtotime('-30 minutes'))],
-        ])->update([
+        $where = [['create_at', '<', date('Y-m-d H:i:s', strtotime('-30 minutes'))]];
+        $count = Db::name('StoreOrder')->where(['pay_state' => '0'])->whereIn('status', ['1', '2'])->where($where)->update([
             'status'       => '0',
             'cancel_state' => '1',
             'cancel_at'    => date('Y-m-d H:i:s'),
             'cancel_desc'  => '30分钟未完成支付自动取消订单',
         ]);
         if ($count > 0) {
-            $this->output->info('auto cancel order -> ' . $count);
+            $this->output->info("共计自动取消了30分钟未支付的{$count}笔订单！");
         } else {
-            $this->output->comment('not cancel order.');
+            $this->output->comment('没有需要自动取消30分钟未支付的订单记录！');
         }
         # 清理一天前未支付的订单
-        $list = Db::name('StoreOrder')->where([
-            ['pay_state', 'eq', '0'],
-            ['create_at', '<', date('Y-m-d H:i:s', strtotime('-1 day'))],
-        ])->limit(20)->select();
+        $where = [['create_at', '<', date('Y-m-d H:i:s', strtotime('-1 day'))]];
+        $list = Db::name('StoreOrder')->where(['pay_state' => '0'])->where($where)->limit(20)->select();
         if (count($order_nos = array_unique(array_column($list, 'order_no'))) > 0) {
-            $this->output->info('auto clear order -> ' . join(',' . PHP_EOL . "\t", $order_nos));
+            $this->output->info("自动删除前一天已经取消的订单：\n\t" . join(',' . PHP_EOL . "\t", $order_nos));
             Db::name('StoreOrder')->whereIn('order_no', $order_nos)->delete();
             Db::name('StoreOrderList')->whereIn('order_no', $order_nos)->delete();
         } else {
-            $this->output->comment('not clear order.');
+            $this->output->comment('没有需要自动删除前一天已经取消的订单！');
         }
     }
 
