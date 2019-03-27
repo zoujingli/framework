e8acb8c480dcf28b56fec369c8b27ca3daaa7e47
Anyon <zoujingli@qq.com>
2019-03-27 16:06:49 +0800

    Update Receive.php

--- application/service/handler/Receive.php
+++ application/service/handler/Receive.php
@@ -37,7 +37,12 @@ class Receive
      */
     public static function handler($appid)
     {
-        p('---handler---  ' . request()->path());
+        p('==================');
+        p('==================');
+        p('==================');
+        p('==================');
+        p('---handler--path---  ' . request()->path());
+        p('---handler--ip  ---  ' . request()->ip());
         try {
             $service = Wechat::WeChatReceive($appid);
         } catch (\Exception $e) {
