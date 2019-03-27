cb7a8752ece65398d4ea5d547712685fc8bc5940
Anyon <zoujingli@qq.com>
2019-03-27 15:59:49 +0800

    Update Receive.php

--- application/service/handler/Receive.php
+++ application/service/handler/Receive.php
@@ -53,7 +53,11 @@ class Receive
             list($data, $openid) = [$service->getReceive(), $service->getOpenid()];
             if (isset($data['EventKey']) && is_object($data['EventKey'])) $data['EventKey'] = (array)$data['EventKey'];
             $input = ['openid' => $openid, 'appid' => $appid, 'receive' => serialize($data), 'encrypt' => intval($service->isEncrypt())];
-            if (is_string($result = http_post($config['appuri'], $input, ['timeout' => 30]))) return $result;
+            if (is_string($result = http_post($config['appuri'], $input, ['timeout' => 30]))) {
+                p('---收到需要返回的内容---');
+                p($result);
+                return $result;
+            }
         } catch (\Exception $e) {
             \think\facade\Log::error("微信{$appid}接口调用异常，{$e->getMessage()}");
         }
