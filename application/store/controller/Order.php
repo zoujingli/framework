881c38b5fec9310dbe54f5893d50026cf24fc132
Anyon <zoujingli@qq.com>
2019-04-01 17:06:04 +0800

    [更新]增加发货操作

--- application/store/controller/Order.php
+++ application/store/controller/Order.php
@@ -67,4 +67,52 @@ class Order extends Controller
         }
     }
 
+    /**
+     * 快递管理
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function express()
+    {
+        if ($this->request->isGet()) {
+            $where = ['is_deleted' => '0', 'status' => '1'];
+            $this->expressList = Db::name('StoreExpress')->where($where)->order('sort asc,id desc')->select();
+        }
+        $this->_form($this->table);
+    }
+
+    /***
+     * 快递追踪查询
+     */
+    public function expressQuery()
+    {
+        list($code, $no) = [input('code', ''), input('no', '')];
+        if (empty($no)) $this->error('快递编号不能为空！');
+        if (empty($code)) $this->error('快递公司编码不能为空！');
+        $this->result = \library\tools\Express::query($code, $no);
+        $this->fetch();
+    }
+
+    /**
+     * 快递表单处理
+     * @param array $vo
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    protected function _express_form_filter(&$vo)
+    {
+        if ($this->request->isPost()) {
+            $order = Db::name($this->table)->where(['id' => $vo['id']])->find();
+            if (empty($order)) $this->error('订单查询异常，请稍候再试！');
+            $express = Db::name('StoreExpress')->where(['express_code' => $vo['express_company_code']])->find();
+            if (empty($express)) $this->error('发货快递公司异常，请重新选择快递公司！');
+            $vo['express_company_title'] = $express['express_title'];
+            $vo['express_send_at'] = empty($order['express_send_at']) ? date('Y-m-d H:i:s') : $order['express_send_at'];
+            $vo['express_state'] = '1';
+            $vo['status'] = '4';
+        }
+    }
+
 }
