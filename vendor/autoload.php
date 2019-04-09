3f1b33cde5d8f254452c556de6b551bdd9539254
Anyon <zoujingli@qq.com>
2019-04-09 10:57:51 +0800

    [更新]同步时更新列表检查接口密钥

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc744955c73f01bab3a33c83f0ef7a7b0::getLoader();
+return ComposerAutoloaderInit8237e77fa5b8b963c72c1fda1d1c7a07::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInitc744955c73f01bab3a33c83f0ef7a7b0
+class ComposerStaticInit8237e77fa5b8b963c72c1fda1d1c7a07
 {
     public static $files = array (
         '841780ea2e1d6545ea3a253239d59c05' => __DIR__ . '/..' . '/qiniu/php-sdk/src/Qiniu/functions.php',
@@ -346,9 +346,9 @@ class ComposerStaticInitc744955c73f01bab3a33c83f0ef7a7b0
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInitc744955c73f01bab3a33c83f0ef7a7b0::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInitc744955c73f01bab3a33c83f0ef7a7b0::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInitc744955c73f01bab3a33c83f0ef7a7b0::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit8237e77fa5b8b963c72c1fda1d1c7a07::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit8237e77fa5b8b963c72c1fda1d1c7a07::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit8237e77fa5b8b963c72c1fda1d1c7a07::$classMap;
 
         }, null, ClassLoader::class);
     }
