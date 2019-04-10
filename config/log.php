1a6c877a04aadf9a139869add24db818211dfaa2
Anyon <zoujingli@qq.com>
2019-04-10 14:26:21 +0800

    [更新]修改日志配置文件

--- config/log.php
+++ config/log.php
@@ -14,9 +14,11 @@
 
 return [
     // 设置日志文件名
-    'single'    => 'single',
-    // 最多保留100个文件
-    'max_files' => 50,
+    'single'      => 'single',
+    // 最多保留50个文件
+    'max_files'   => 50,
     // 日志每10兆分割文件
-    'file_size' => 10485760,
+    'file_size'   => 10485760,
+    // 日志类型分别写入文件
+    'apart_level' => ['error', 'sql'],
 ];
