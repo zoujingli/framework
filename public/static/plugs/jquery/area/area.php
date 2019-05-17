dd7fefaf0338e018ed9f9d05e1c74f269ca6b003
Anyon <zoujingli@qq.com>
2019-05-17 10:51:40 +0800

    [更新]修改后台省市区插件生成工具

--- public/static/plugs/jquery/area/area.php
+++ public/static/plugs/jquery/area/area.php
@@ -3,8 +3,10 @@
 $data = [];
 $list = json_decode(file_get_contents(__DIR__ . '/area.json'), true);
 foreach ($list as $citys) {
-    $line = $citys['name'];
-    foreach ($citys['list'] as $area) $line = $line . '$' . $area['name'] . ',' . join(',', $area['list']);
-    $data[] = $line;
+    $lines = [];
+    foreach ($citys['list'] as $area) {
+        $lines[] = $area['name'] . ',' . join(',', $area['list']);
+    }
+    $data[] = $citys['name'] . '$' . join('|',$lines);
 }
 echo join('#', $data);
